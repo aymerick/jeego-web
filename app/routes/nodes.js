0055626603cbc6c031c92c34ee34e32ac405afa2
Aymerick <aymerick@jehanne.org>
2014-06-06 19:09:16 +0200

    comment

--- app/routes/nodes.js
+++ app/routes/nodes.js
@@ -2,4 +2,10 @@ export default Ember.Route.extend({
   model: function(){
     return this.store.find('node');
   }
+
+  // NOTE: seems broken (cf. http://discuss.emberjs.com/t/using-redirect-for-default-child-route-broken-in-1-4/4681)
+  //       fixed here: https://github.com/emberjs/ember.js/commit/19ed311952fb85c0921e644a555b9a3ceeeeaab8
+  // redirect: function(model) {
+  //   return this.transitionTo('node', model.get('firstObject'));
+  // }
 });
