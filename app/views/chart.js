c20410f8f177be2e36e222618e366d3bd2fcea32
Aymerick <aymerick@jehanne.org>
2014-06-10 16:29:39 +0200

    cleanup

--- app/views/chart.js
+++ app/views/chart.js
@@ -1,57 +1,57 @@
 export default Ember.View.extend({
 
-	series: [ ],
-	title: null,
-	yAxisTitle: null,
+  series: [ ],
+  title: null,
+  yAxisTitle: null,
 
-	highchartsSettings: function() {
-		var result = {
-			xAxis: {
-				type: 'datetime'
-			},
+  highchartsSettings: function() {
+    var result = {
+      xAxis: {
+        type: 'datetime'
+      },
 
-			tooltip: {
-				crosshairs: true,
-				shared: true,
-			},
+      tooltip: {
+        crosshairs: true,
+        shared: true,
+      },
 
-			legend: {
-				layout: 'vertical',
-				align: 'right',
-				verticalAlign: 'middle',
-				borderWidth: 0
-			}
-		};
+      legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle',
+        borderWidth: 0
+      }
+    };
 
-		if (!Ember.empty(this.get('title'))) {
-			result['title'] = { 'text': this.get('title') };
-		}
+    if (!Ember.empty(this.get('title'))) {
+      result['title'] = { 'text': this.get('title') };
+    }
 
-		if (!Ember.empty(this.get('yAxisTitle'))) {
-			result['yAxis'] = { 'title': { text: this.get('yAxisTitle') } };
-		}
+    if (!Ember.empty(this.get('yAxisTitle'))) {
+      result['yAxis'] = { 'title': { text: this.get('yAxisTitle') } };
+    }
 
-		if (!Ember.empty(this.get('series'))) {
-			result['series'] = this.get('series');
-		}
+    if (!Ember.empty(this.get('series'))) {
+      result['series'] = this.get('series');
+    }
 
-		if (!Ember.empty(this.get('tooltipSuffix'))) {
-			result['tooltip']['valueSuffix'] = this.get('tooltipSuffix');
-		}
+    if (!Ember.empty(this.get('tooltipSuffix'))) {
+      result['tooltip']['valueSuffix'] = this.get('tooltipSuffix');
+    }
 
-		return result;
-	},
+    return result;
+  },
 
   didInsertElement: function() {
-  	this._super();
+    this._super();
 
     this.$().highcharts(this.highchartsSettings());
   },
 
   willDestroyElement: function() {
-  	this._super();
+    this._super();
 
-  	this.$().highcharts().destroy();
+    this.$().highcharts().destroy();
   }
 
 });
