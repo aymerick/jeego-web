32e1833df9912ef085b87f73af6f319e69ca9c5d
Aymerick <aymerick@jehanne.org>
2014-06-11 11:24:43 +0200

    Display chart with a component instead of a view

---
+++ app/components/temperature-chart.js
@@ -0,0 +1,46 @@
+export default Ember.Component.extend({
+  chartSettings: function() {
+    var result = {
+      title: {
+        text: "Temperature"
+      },
+
+      xAxis: {
+        type: 'datetime'
+      },
+
+      yAxis: {
+        'title': {
+          text: 'Temperature (°C)'
+        }
+      },
+
+      tooltip: {
+        valueSuffix: '°C',
+        crosshairs: true,
+        shared: true
+      },
+
+      legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle',
+        borderWidth: 0
+      }
+    };
+
+    if (!Ember.empty(this.get('series'))) {
+      result['series'] = this.get('series');
+    }
+
+    return result;
+  },
+
+  initializeChart: function() {
+    this.$().highcharts(this.chartSettings());
+  }.on('didInsertElement'),
+
+  destroyChart: function() {
+    this.$().highcharts().destroy();
+  }.on('willDestroyElement')
+});
