6d87272eca2f8055bd3d033534ffa65c5c8623c0
Aymerick <aymerick@jehanne.org>
2014-06-12 11:19:25 +0200

    Display correct sensors

--- app/models/node.js
+++ app/models/node.js
@@ -4,6 +4,8 @@ var Node = DS.Model.extend({
   last_seen_at: DS.attr('iso-date'),
   name:         DS.attr('string'),
   domoticz_idx: DS.attr('string'),
+
+  // sensors
   temperature:  DS.attr('number'),
   humidity:     DS.attr('number'),
   light:        DS.attr('number'),
@@ -12,8 +14,49 @@ var Node = DS.Model.extend({
   vcc:          DS.attr('number')
 });
 
+// class methods and properties
 Node.reopenClass({
-  SENSORS: [ 'temperature', 'humidity', 'light', 'motion', 'low_battery', 'vcc' ]
+  SENSORS: [ 'temperature', 'humidity', 'light', 'motion', 'low_battery', 'vcc' ],
+
+  // @todo Receive that table from the server
+  SENSORS_FOR_KIND: {
+    1: [ 'temperature', 'humidity', 'light', 'motion', 'low_battery' ],
+    2: [ 'temperature', 'humidity', 'light', 'low_battery' ],
+    3: [ 'temperature', 'vcc' ],
+    4: [ 'temperature', 'humidity', 'vcc' ],
+    5: [ 'temperature', 'light', 'vcc' ]
+  }
+});
+
+// instance methods and properties
+Node.reopen({
+  hasSensor: function(sensor) {
+    return Node.SENSORS_FOR_KIND[this.get('kind')].contains(sensor);
+  },
+
+  hasTemperature: function() {
+    return this.hasSensor('temperature');
+  }.property('kind'),
+
+  hasHumidity: function() {
+    return this.hasSensor('humidity');
+  }.property('kind'),
+
+  hasLight: function() {
+    return this.hasSensor('light');
+  }.property('kind'),
+
+  hasMotion: function() {
+    return this.hasSensor('motion');
+  }.property('kind'),
+
+  hasLowBattery: function() {
+    return this.hasSensor('low_battery');
+  }.property('kind'),
+
+  hasVcc: function() {
+    return this.hasSensor('vcc');
+  }.property('kind')
 });
 
 export default Node;
--- app/routes/node/temperature.js
+++ app/routes/node/temperature.js
@@ -5,11 +5,12 @@ export default Ember.Route.extend({
     var url     = adapter.get('host');
 
     if (!Ember.isEmpty(adapter.get('namespace'))) {
-      url += '/' + adapter.get('namespace')
+      url += '/' + adapter.get('namespace');
     }
 
     url += '/nodes/' + node.get('id') + '/temperatures';
 
+    // @todo Setup a hasMany relationship on a new Log model, with a computed filter to get all temperatures logs
     return $.getJSON(url).then(function(data) {
       return { 'series': [{
         'name': node.get('name'),
